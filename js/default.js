08b2ab11972bbe3689c316df7f57eab712e411e1
konohanaruto <kingshi2010@gmail.com>
2018-01-10 13:01:16 +0000

    添加发送动态和文字传情的两个modal

--- js/default.js
+++ js/default.js
@@ -39,6 +39,50 @@ $.fn.getCharacterLength = function getByteLen(val) {
 }
 
 $(function () {
+
+    function adjustHeight(el, minHeight) {
+        // compute the height difference which is caused by border and outline
+        var outerHeight = parseInt(window.getComputedStyle(el).height, 10);
+        var diff = outerHeight - el.clientHeight;
+
+        // set the height to 0 in case of it has to be shrinked
+        el.style.height = 0;
+
+        // set the correct height
+        // el.scrollHeight is the full height of the content, not just the visible part
+        el.style.height = Math.max(minHeight, el.scrollHeight + diff) + 'px';
+    }
+
+
+    // we use the "data-adaptheight" attribute as a marker
+    var textAreas = [].slice.call(document.querySelectorAll('textarea[data-adaptheight]'));
+
+    // iterate through all the textareas on the page
+    textAreas.forEach(function(el) {
+
+        // we need box-sizing: border-box, if the textarea has padding
+        el.style.boxSizing = el.style.mozBoxSizing = 'border-box';
+
+        // we don't need any scrollbars, do we? :)
+        el.style.overflowY = 'hidden';
+
+        // the minimum height initiated through the "rows" attribute
+        var minHeight = el.scrollHeight;
+
+        el.addEventListener('input', function() {
+            adjustHeight(el, minHeight);
+        });
+
+        // we have to readjust when window size changes (e.g. orientation change)
+        window.addEventListener('resize', function() {
+            adjustHeight(el, minHeight);
+        });
+
+        // we adjust height to the initial content
+        adjustHeight(el, minHeight);
+
+    });
+
     var homePublishTrendTextarea = $('.publish-trend-modal textarea.js-trend-content');
     $('.publish-trend-modal .tag-list .js-add-tag').on('click', function () {
         $(this).parents('.publish-trend-modal').find('.js-placeholder').addClass('hide');
